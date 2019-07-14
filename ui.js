ec2e7c7bf5e042c065ac49b5468c90c0aa69a71e
Kris Lafrance <Kris.lafrance6067@gmail.com>
2019-07-14 00:09:54 -0700

    able to display data on screen as well as some qol

--- ui.js
+++ ui.js
@@ -9,17 +9,18 @@ class UI {
     this.maxTemp = document.getElementById('w-maxtemp');
     this.pressure = document.getElementById('w-pressure');
     this.wind = document.getElementById('w-wind');
+    this.description = document.getElementById('w-description');
     this.coords = document.getElementById('w-coords');
   }
-    
   
   drawResults(weather) {
   this.location.textContent = weather.name;
-  this.desc.textContent = weather.description;
-  this.string.textContent = weather.main.temp + "°C";
+  // Need to capitilize first letter
+  this.desc.textContent = `${weather.weather[0].description}`;
+  this.string.textContent = `${Math.trunc(weather.main.temp)} °C`;
   this.icon.setAttribute('src', `http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`);
   this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}%`;
-  this.maxTemp.textContent = `Max Temperature: ${weather.main.temp_max}`;
+  this.maxTemp.textContent = `Max Temperature: ${Math.trunc(weather.main.temp_max)} °C`;
   this.pressure.textContent = `Pressure: ${weather.main.pressure} hpa`;
   this.wind.textContent = `Wind Speed: ${weather.wind.speed} m/s`;
   this.coords.textContent = `Geo Coords: ${weather.coord.lon}, ${weather.coord.lat}`;
